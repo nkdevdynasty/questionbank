99d6ff19bcd92ecdf9f38d9fb9673317d3d4be89
myselfpratyush <156774989+myselfpratyush@users.noreply.github.com>
2024-02-07 00:09:52 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,14 @@ import { custom } from 'zod';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Nk Nagar',
 			customCss: ['./src/content/custom-styles/custom.css'],
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Pages',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', link: '/guides/example/' },
